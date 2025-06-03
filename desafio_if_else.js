502792be4078c10d7c0442d0f98e6dedf7165354
gabriela <blinkeaguilera@gmail.com>
2025-06-03 11:28:33 -0300

    desafio_if_else

---
+++ desafio_if_else.js
@@ -0,0 +1,197 @@
+let respostaCorreta = 0
+let respostaIncorreta = 0
+
+// Pergunta 1
+console.log(&quot;Em qual ano estamos?&quot;)
+console.log(&quot;A - 2050&quot;)
+console.log(&quot;B - 2025&quot;)
+console.log(&quot;C - 2030&quot;)
+console.log(&quot;D - 2060&quot;)
+let pergunta1 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta1 === &quot;B&quot; || pergunta1 === &quot;2025&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+// Pergunta 2
+console.log(&quot;\nQual é o resultado de 2 + 2?&quot;)
+console.log(&quot;A - 3&quot;)
+console.log(&quot;B - 5&quot;)
+console.log(&quot;C - 4&quot;)
+console.log(&quot;D - 6&quot;)
+let pergunta2 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta2 === &quot;C&quot; || pergunta2 === &quot;4&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+// Pergunta 3
+console.log(&quot;\nQual é a cor do céu em um dia claro?&quot;)
+console.log(&quot;A - Verde&quot;)
+console.log(&quot;B - Azul&quot;)
+let pergunta3 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta3 === &quot;B&quot; || pergunta3 === &quot;Azul&quot;) { // &lt;-- Chave de abertura correta aqui
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+//Pergunta4
+console.log(&quot;\nQual desses animais pode voar?&quot;)
+console.log(&quot;A - Cachorro&quot;)
+console.log(&quot;B - Peixe&quot;)
+console.log(&quot;C - Pássaro&quot;)
+console.log(&quot;D - Cavalo&quot;)
+let pergunta4 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta4 === &quot;C&quot; || pergunta4 === &quot;Pássaro&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++;
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+//Pergunta 5
+console.log(&quot;\nQuantos dias tem uma semana?&quot;)
+console.log(&quot;A - 5&quot;)
+console.log(&quot;B - 6&quot;)
+console.log(&quot;C - 7&quot;)
+console.log(&quot;D - 8&quot;)
+let pergunta5 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta5 === &quot;C&quot; || pergunta5 === &quot;7&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+// Pergunta 6
+console.log(&quot;\nQual é o maior planeta do Sistema Solar?&quot;)
+
+console.log(&quot;A - Marte&quot;)
+console.log(&quot;B - Terra&quot;)
+console.log(&quot;C - Júpiter&quot;)
+console.log(&quot;D - Vênus&quot;)
+let pergunta6 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta6 === &quot;C&quot; || pergunta6 === &quot;Júpiter&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+//Pergunta 7
+console.log(&quot;\nQual destes alimentos é uma fruta?&quot;)
+console.log(&quot;A - Cenoura&quot;)
+console.log(&quot;B - Maçã&quot;)
+console.log(&quot;C - Alface&quot;)
+console.log(&quot;D - Batata&quot;)
+let pergunta7 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta7 === &quot;B&quot; || pergunta7 === &quot;Maçã&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+//Pergunta 8
+console.log(&quot;\nO que usamos para escovar os dentes?&quot;)
+console.log(&quot;A - Garfo&quot;)
+console.log(&quot;B - Escova de dentes&quot;)
+console.log(&quot;C - Martelo&quot;)
+console.log(&quot;D - Prato&quot;)
+let pergunta8 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta8 === &quot;B&quot; || pergunta8 === &quot;Escova de dentes&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+//pergunta 9
+console.log(&quot;\nEm que continente fica o Brasil?&quot;)
+console.log(&quot;A - Europa&quot;)
+
+console.log(&quot;B - América do Sul&quot;)
+console.log(&quot;C - África&quot;)
+console.log(&quot;D - Ásia&quot;)
+let pergunta9 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta9 === &quot;B&quot; || pergunta9 === &quot;América do Sul&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+}
+// Exibir Resultado
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
+
+//pergunta 10
+console.log(&quot;\nQual desses líquidos é essencial para a vida?&quot;)
+console.log(&quot;A - Água&quot;)
+console.log(&quot;B - Óleo&quot;)
+console.log(&quot;C - Refrigerante&quot;)
+console.log(&quot;D - Suco&quot;)
+let pergunta10 = prompt(&quot;Digite a alternativa correta:&quot;)
+
+if (pergunta10 === &quot;A&quot; || pergunta10 === &quot;Água&quot;) {
+console.log(&quot;Alternativa correta!&quot;)
+respostaCorreta++
+} else {
+console.log(&quot;Alternativa errada!&quot;)
+respostaIncorreta++
+
+}
+
+// Exibir Resultado Final
+console.log(&quot;\nResumo Final&quot;)
+console.log(&quot;Respostas corretas: &quot; + respostaCorreta)
+console.log(&quot;Respostas incorretas: &quot; + respostaIncorreta)
